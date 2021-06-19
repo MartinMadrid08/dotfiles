97c0980d34cb20c3bc335cb86c4d8669b4744ac9
MartinMadrid08 <81242472+MartinMadrid08@users.noreply.github.com>
2021-06-19 17:16:59 -0400

    Add files via upload

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -11,10 +11,11 @@ xrandr --output eDP1 --primary --mode 1366x768 --rotate normal --output HDMI1 --
 killall -q polybar &
 xinput disable 15 &
 
-nitrogen --restore &
+feh --bg-fill /home/martin/Imágenes/walpaper.jpg &
+picom --config ~/.config/picom/picom.conf &
 xsetroot -cursor_name left_ptr &
 run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
-picom --config $HOME/.config/bspwm/picom.conf &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+
 polybar eDP1 &
 polybar HDMI1 &
